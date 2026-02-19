144f1a8274c5aadce9da3fee82e73ad0a3e01941
TeleAI-mcp <teleaitest@163.com>
2026-02-19 19:38:01 +0800

    Copy Django global_settings.py from django/django repository

---
+++ django/conf/global_settings.py
@@ -0,0 +1,7 @@
+#
+# This is an auto-generated Django settings module.
+# You shouldn't edit it; instead, copy it to your project and edit that copy.
+# For more information on this file, see
+# https://docs.djangoproject.com/en/stable/topics/settings/
+# For the full list of settings and their values, see
+# https://docs.djangoproject.com/en/stable/ref/settings/
